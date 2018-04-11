1dad15cda81ed629fc5e9c951c63db005fce2775
Maximo DeLaRosa <mjd809@gmail.com>
2018-04-10 22:39:02 -0400

    Added more dummy text
    Noticed a bit of textual error-text overflowing. Must fix

--- src/App.js
+++ src/App.js
@@ -23,33 +23,33 @@ export default class App extends React.Component {
     {
       id: 2,
       title: "Bushwick Aesthetic",
-      body: "Welcome to learning React!"
+      body: "Vice chicharrones nulla gluten-free. Humblebrag magna flexitarian deep v selvage, unicorn health goth blog chia trust fund semiotics bushwick adipisicing brunch. Readymade pug mumblecore ipsum occaecat, dolor taxidermy. Cold-pressed aute ut knausgaard pitchfork, laboris PBR&B. Jean shorts subway tile pitchfork forage aliqua. Coloring book jianbing tempor locavore, williamsburg umami brooklyn fingerstache wayfarers meditation ullamco dolore pop-up hoodie."
     },
-    { id: 3, title: "Unicorn tbh", body: "Welcome to learning React!" },
+    { id: 3, title: "Unicorn tbh", body: " Enamel pin intelligentsia leggings, messenger bag vexillologist literally fanny pack banh mi bushwick synth bicycle rights paleo man bun heirloom." },
     {
       id: 4,
       title: "Truffaut Chillwave",
-      body: "Welcome to learning React!"
+      body: " DIY cold-pressed offal, gochujang sartorial proident VHS af meditation chillwave flannel. Ennui nulla marfa, hell of deserunt cray exercitation neutra viral celiac +1 man braid nostrud. Tote bag next level dolor fixie, fashion axe tousled raw denim you probably haven't heard of them try-hard schlitz roof party retro before they sold out adipisicing. Incididunt brunch helvetica lyft chillwave artisan chicharrones adipisicing squid ullamco."
     },
-    { id: 5, title: "Health Goth", body: "You can install React from npm." 
+    { id: 5, title: "Health Goth", body: "Bicycle rights artisan gochujang meditation swag, four loko tofu banjo actually. Succulents 90's swag iceland hoodie. Farm-to-table meh tote bag DIY chambray jean shorts succulents semiotics brooklyn literally pitchfork tousled small batch drinking vinegar man bun. Jianbing shoreditch synth hammock farm-to-table. Seitan air plant woke trust fund tilde glossier bitters." 
     },  {
       id: 6,
       title: "Paleo DIY",
       body:
-        "Weffffff fffffff ffffffff ffflfgdfgd gggggggg gcome fffffffff ffflfgdfgd gggg to learning React!"
+        "Waistcoat microdosing synth bicycle rights YOLO yuccie post-ironic photo booth lo-fi. Roof party pug PBR&B food truck schlitz brunch ethical flannel. Kitsch fingerstache roof party cornhole vaporware aesthetic. Poutine venmo cronut, lyft man bun master cleanse freegan offal mlkshk pork belly. Cliche jianbing occupy raclette kickstarter mlkshk irony banjo butcher. Bushwick actually skateboard, iPhone hoodie blog activated charcoal hashtag hot chicken try-hard health goth thundercats umami cornhole. Umami edison bulb neutra kogi cold-pressed tilde listicle."
     },
     {
       id: 7,
-      title: "Bushwick Aesthetic",
-      body: "Welcome to learning React!"
+      title: "Vinegar Man Bun",
+      body: " Cliche jianbing occupy raclette kickstarter mlkshk irony banjo butcher. Bushwick actually skateboard, iPhone hoodie blog activated charcoal hashtag hot chicken try-hard health goth thundercats umami cornhole. Umami edison bulb neutra kogi cold-pressed tilde listicle."
     },
-    { id: 8, title: "Unicorn tbh", body: "Welcome to learning React!" },
+    { id: 8, title: "Unicorn tbh", body: "knausgaard selfies woke waistcoat gluten-free tousled seitan venmo mixtape tattooed actually kombucha. Yuccie fixie kickstarter, cardigan try-hard hexagon keffiyeh aesthetic meh venmo. " },
     {
       id: 9,
-      title: "Truffaut Chillwave",
-      body: "Welcome to learning React!"
+      title: "Master cleanse",
+      body: "Dreamcatcher adipisicing do, subway tile migas ex irony raclette quis kogi magna butcher artisan venmo chillwave. Gluten-free raw denim brunch small batch, scenester shaman sustainable fixie kitsch street art. Umami ea excepteur etsy keytar tumeric. Street art before they sold out unicorn, occaecat poutine fam la croix neutra meggings tote bag jianbing stumptown DIY."
     },
-    { id: 10, title: "Health Goth", body: "You can install React from npm." 
+    { id: 10, title: "Iceland Cheers", body: "Cred man bun iceland, chicharrones pork belly chia skateboard af. Wayfarers tofu hella, tousled plaid art party small batch 8-bit vexillologist synth listicle. Marfa tofu tilde farm-to-table humblebrag taiyaki kickstarter, bushwick " 
     }
   ]
   };
