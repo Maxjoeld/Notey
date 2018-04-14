e8a9b59700ea141423b3bef8ccb301607f1df2a3
Maximo DeLaRosa <mjd809@gmail.com>
2018-04-14 16:40:39 -0400

    Completed the sign in page

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,34 +1,29 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./SideBar.css";
-import FontAwesome from "react-fontawesome";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import FontAwesome from 'react-fontawesome';
+import './SideBar.css';
 
-class SideBar extends Component {
-  render() {
-    return (
-      <div className="SideBar">
-        <h1 className="SideBar__title">
-          Notey <FontAwesome name="fas fa-book" /><br />DataBase
-        </h1>
-        <Link to={"/"}>
-          <div className="SetIc">
-            <FontAwesome name="fas fa-sticky-note" />
-            <button className="SideBar__button">
-              View Your Notes
-            </button>
-          </div>
-        </Link>
-        <Link to={"/create"}>
-          <div className="SetIc">
+const SideBar = () => {
+  return (
+    <div className="SideBar">
+      <h1 className="SideBar__title">
+        Notey <FontAwesome name="fas fa-book" />
+        <br />DataBase
+      </h1>
+      <Link to="/">
+        <div className="SetIc">
+          <FontAwesome name="fas fa-sticky-note" />
+          <button className="SideBar__button">View Your Notes</button>
+        </div>
+      </Link>
+      <Link to="/create">
+        <div className="SetIc">
           <FontAwesome name="fas fa-plus" />
-            <button className="SideBar__button">
-              Create New Note
-            </button>
-          </div>
-        </Link>
-      </div>
-    );
-  }
-}
+          <button className="SideBar__button">Create New Note</button>
+        </div>
+      </Link>
+    </div>
+  );
+};
 
 export default SideBar;
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import './SignUp.css';
+import logo from './google.png';
 
 class SignUp extends Component {
-  state = {}
+  state = {};
   render() {
     return (
-      <div className=''>
-        <h1 className='header'>Hello worllllddd</h1>
+      <div className="signin">
+        <div className="signin--box">
+          <h1 className="signin--header">Sign in With</h1>
+          <div className="signin--buttons">
+            <button className="signin--buttons__facebook">
+              <i className="fab fa-facebook-square" />facebook
+            </button>
+            <button className="signin--buttons__google">
+              <img
+                src={logo}
+                alt="google logo"
+                className="signin--buttons__google--logo"
+              />Google
+            </button>
+          </div>
+          <form className="signin--signin">
+            Username:<br />
+            <input className="signin--signin__username" placeholder="Username" />
+            <br />
+            Password
+            <br />
+            <input className="signin--signin__password" placeholder="Password" />
+            <br />
+            <input className="signin--signin__button" type="submit" value="Sign In" />
+          </form>
+          <p className="signin--notmember"> Not a member? Sign up now</p>
+        </div>
       </div>
     );
   }
